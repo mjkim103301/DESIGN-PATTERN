206c6a08f2a37ae90b26da40d644f66232094bca
mjkim103301 <mjkim103301@gmail.com>
2022-06-26 23:28:10 +0900

    feat: template method pattern

---
+++ src/a08/template_method_pattern/Tea.java
@@ -0,0 +1,10 @@
+package a08.template_method_pattern;
+
+public class Tea extends CaffeineBeverage {
+	public void brew() {
+		System.out.println("Steeping the tea");
+	}
+	public void addCondiments() {
+		System.out.println("Adding Lemon");
+	}
+}
